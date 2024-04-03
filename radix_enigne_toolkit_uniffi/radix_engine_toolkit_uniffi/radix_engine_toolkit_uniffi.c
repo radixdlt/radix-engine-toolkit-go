01b4131e2dfdd05f5d96a488ca42eaaa6c738e57
Michał Strug <michal.strug@rdx.works>
2024-04-03 16:00:25 +0200

    Cleanup

--- radix_enigne_toolkit_uniffi/radix_engine_toolkit_uniffi/radix_engine_toolkit_uniffi.c
+++
@@ -1,8 +0,0 @@
-#include <radix_engine_toolkit_uniffi.h>
-
-// This file exists beacause of
-// https://github.com/golang/go/issues/11263
-
-void cgo_rust_task_callback_bridge_radix_engine_toolkit_uniffi(RustTaskCallback cb, const void * taskData, int8_t status) {
-  cb(taskData, status);
-}
